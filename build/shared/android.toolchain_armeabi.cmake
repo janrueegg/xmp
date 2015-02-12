f507cc2e831fda96abda9db619828d49a67a1e59
Jan Rüegg <jan.rueegg@dacuda.com>
2015-02-12 15:49:15 +0100

    Added x86 and armeabi builds for android

---
+++ build/shared/android.toolchain_armeabi.cmake
@@ -0,0 +1,4 @@
+set(ANDROID_TOOLCHAIN_NAME "arm-linux-androideabi-4.8")
+set(ANDROID_ABI "armeabi-v7a with NEON")
+set(ANDROID_NATIVE_API_LEVEL "17")
+include(shared/android.toolchain.cmake)
---
+++ build/shared/android.toolchain_x86.cmake
@@ -0,0 +1,4 @@
+set(ANDROID_TOOLCHAIN_NAME "x86-4.8")
+set(ANDROID_ABI "x86")
+set(ANDROID_NATIVE_API_LEVEL "17")
+include(shared/android.toolchain.cmake)
